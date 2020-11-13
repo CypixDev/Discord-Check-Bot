a235d0f016effd860324715976e1a200ff8a2043
Cypix <pi.konermann@gmail.com>
2020-11-13 14:12:10 +0100

    log ready in Logger

--- src/main/java/de/cypix/tasks_check_bot/events/ReadyListener.java
+++ src/main/java/de/cypix/tasks_check_bot/events/ReadyListener.java
@@ -1,5 +1,6 @@
 package de.cypix.tasks_check_bot.events;
 
+import de.cypix.tasks_check_bot.main.TasksCheckBot;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
@@ -10,6 +11,6 @@ public class ReadyListener implements EventListener {
     public void onEvent(GenericEvent event)
     {
         if (event instanceof ReadyEvent)
-            System.out.println("API is ready!");
+            TasksCheckBot.logger.info("API is ready!");
     }
 }
